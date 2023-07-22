9dc4702123f5294a89db8e60994cc3f23da7d902
Nikola Petrovic <s.nino.petrovic@gmail.com>
2023-07-22 10:40:37 +0200

    feat: added ReservationStatus type

--- models/reservations.go
+++ models/reservations.go
@@ -8,10 +8,20 @@ import (
 
 type Reservation struct {
 	gorm.Model
-	ID             int64     `json:"id"`
-	AccomodationId int64     `json:"accomodation_id"`
-	UserId         int64     `json:"user_id"`
-	StartDate      time.Time `json:"start_date"`
-	EndDate        time.Time `json:"end_date"`
-	Status         int32     `json:"status"`
+	ID             int64             `json:"id"`
+	AccomodationId int64             `json:"accomodation_id"`
+	NumberOfGuests int64             `json:"number_of_guests"`
+	UserId         int64             `json:"user_id"`
+	StartDate      time.Time         `json:"start_date"`
+	EndDate        time.Time         `json:"end_date"`
+	Status         ReservationStatus `json:"status"`
 }
+
+type ReservationStatus int32
+
+const (
+	UNSPECIFIED ReservationStatus = 0
+	ACTIVE      ReservationStatus = 1
+	ACCEPTED    ReservationStatus = 2
+	DECLINED    ReservationStatus = 3
+)
--- services/reservation.go
+++ services/reservation.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"log"
 	"time"
 
 	pb "github.com/dzoniops/common/pkg/reservation"
@@ -22,14 +23,12 @@ func (s *Server) ActivateReservationsGuest(
 	c context.Context,
 	req *pb.IdRequest,
 ) (*pb.ActiveReservationsResponse, error) {
+	var reservations []models.Reservation
+
+	db.DB.Where(&models.Reservation{Status: models.ACTIVE, UserId: req.Id}).
+		Find(&reservations)
 	return &pb.ActiveReservationsResponse{
-		Reservations: []*pb.Reservation{{
-			AccommodationId: 0,
-			StartDate:       &timestamppb.Timestamp{},
-			EndDate:         &timestamppb.Timestamp{},
-			NumberOfGuests:  0,
-			Status:          1,
-		}},
+		Reservations: mapToPb(reservations),
 	}, nil
 }
 
@@ -48,8 +47,12 @@ func (s *Server) Reserve(c context.Context, req *pb.ReserveRequest) (*pb.Reserve
 		UserId:         req.UserId,
 		StartDate:      req.Reservation.StartDate.AsTime(),
 		EndDate:        req.Reservation.EndDate.AsTime(),
-		Status:         int32(req.Reservation.Status),
+		Status:         models.ReservationStatus(req.Reservation.Status),
+		NumberOfGuests: req.Reservation.NumberOfGuests,
 	}
+	// if reservation.StartDate.Before(time.Now()) || reservation.EndDate.Before(time.Now()) {
+	// 	return nil, status.Error(codes.InvalidArgument, "Start or End date have to be in future")
+	// }
 	if reservation.EndDate.Before(reservation.StartDate) {
 		return nil, status.Error(codes.InvalidArgument, "Start date has to be before End date")
 	}
@@ -67,7 +70,24 @@ func (s *Server) Reserve(c context.Context, req *pb.ReserveRequest) (*pb.Reserve
 
 func (s *Server) checkActiveReservations(startDate, endDate time.Time) bool {
 	var reservations []models.Reservation
-	db.DB.Where("start_date > ? AND start_date > ? AND status = 2", startDate, endDate).
-		Or("end_date < ? AND end_date < ? AND status = 2", startDate, endDate).Find(&reservations)
+	db.DB.Where("start_date < ? and end_date > ? and status = ?", endDate, startDate, models.ACCEPTED).
+		Find(&reservations)
+	// db.DB.Where("start_date > ? AND start_date > ? AND status = 2", startDate, endDate).
+	// 	Or("end_date < ? AND end_date < ? AND status = 2", startDate, endDate).Find(&reservations)
+	log.Printf("%v", reservations)
 	return len(reservations) == 0
+}
+
+func mapToPb(in []models.Reservation) []*pb.Reservation {
+	vals := make([]*pb.Reservation, len(in))
+	for i := range in {
+		vals[i] = &pb.Reservation{
+			AccommodationId: in[i].AccomodationId,
+			StartDate:       timestamppb.New(in[i].StartDate),
+			EndDate:         timestamppb.New(in[i].EndDate),
+			NumberOfGuests:  in[i].NumberOfGuests,
+			Status:          pb.ReservationStatus(in[i].Status),
+		}
+	}
+	return vals
 }
